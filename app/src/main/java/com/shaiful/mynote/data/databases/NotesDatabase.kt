730521a9b3a3ae16f3b6c7b8c185a31eebf376cf
shaiful00000 <md.shaiful@apsissolutions.com>
2025-01-15 16:01:13 +0600

    Habbit viewmodel implemented

--- app/src/main/java/com/shaiful/mynote/data/databases/NotesDatabase.kt
+++ app/src/main/java/com/shaiful/mynote/data/databases/NotesDatabase.kt
@@ -8,15 +8,17 @@ import com.shaiful.mynote.data.daos.HabitCheckedDateDao
 import com.shaiful.mynote.data.daos.HabitDao
 import com.shaiful.mynote.data.daos.NoteDao
 import com.shaiful.mynote.data.tables.Category
+import com.shaiful.mynote.data.tables.Habit
+import com.shaiful.mynote.data.tables.HabitCheckedDates
 import com.shaiful.mynote.data.tables.Note
 
 
 @Database(
-    entities = [Category::class, Note::class],
-    version = 1,
+    entities = [Category::class, Note::class, Habit::class, HabitCheckedDates::class],
+    version = 2,
     exportSchema = true,
     autoMigrations = [
-//        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 1, to = 2),
 //        AutoMigration(from = 2, to = 3)
     ]
 )
